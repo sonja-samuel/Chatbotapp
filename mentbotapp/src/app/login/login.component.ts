0baf3b0a677d9814cfdc284688faa7de1c3e1fd5
annasonj <sonja.samuel@daiviktech.com>
2022-03-02 16:38:20 +0530

    updated with login and register backend

--- mentbotapp/src/app/login/login.component.ts
+++ mentbotapp/src/app/login/login.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
 export class LoginComponent implements OnInit{
-    form: any ={
-        username: null,
-        password: null
-    };
+
+    constructor(
+        private authService: AuthService,
+         private tokenStorage: TokenStorageService,
+          private lb: FormBuilder,
+          public router: Router){ }
+
+    loginForm = this.lb.group({
+        username: [null, [Validators.required]],
+        password: [null, [Validators.required]]
+    });
     isLoggedIn = false;
     isLoginFailed= false;
     errorMessage='';
-    roles: string[]=[];
-    constructor(private authService: AuthService, private tokenStorage: TokenStorageService){ }
+    roles: any;
 
+   
     ngOnInit(): void {
         if(this.tokenStorage.getToken()){
             this.isLoggedIn=true;
@@ -24,8 +33,9 @@ export class LoginComponent implements OnInit{
 
         }
     }
-    onSubmit(): void{
-        const{username,password}= this.form;
+    login(): void{
+        const username = this.loginForm.get(['username'])!.value;
+        const password = this.loginForm.get(['password'])!.value;
         this.authService.login(username,password).subscribe(
             data => {
                 this.tokenStorage.saveToken(data.accessToken);
@@ -33,7 +43,15 @@ export class LoginComponent implements OnInit{
                 this.isLoginFailed=false;
                 this.isLoggedIn=true;
                 this.roles=this.tokenStorage.getUser().roles;
-                this.reloadPage();
+                if(this.roles.includes("ROLE_PATIENT")){
+                    
+                    this.router.navigate(['/patients']);
+                } else if(this.roles.includes("ROLE_DOCTOR")){
+                    this.router.navigate(['/doctors']);
+                } else if(this.roles.includes("ROLE_ADMIN")){
+                    this.router.navigate(['/admin']);
+                }
+               
             },
             err => {
                 this.errorMessage=err.error.message;
@@ -44,7 +62,5 @@ export class LoginComponent implements OnInit{
         );
 
     }
-    reloadPage(): void{
-        window.location.reload();
-    }
+   
 }
