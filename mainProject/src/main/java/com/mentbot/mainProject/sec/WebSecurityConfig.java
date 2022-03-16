cc26f148be7e1ebeb01a8d7a8907a76f613ef5f2
annasonj <sonja.samuel@daiviktech.com>
2022-03-16 15:00:14 +0530

    web config changes

--- mainProject/src/main/java/com/mentbot/mainProject/sec/WebSecurityConfig.java
+++ mainProject/src/main/java/com/mentbot/mainProject/sec/WebSecurityConfig.java
@@ -60,7 +60,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 		
 		.authorizeRequests().antMatchers("/api/auth/**").permitAll()
-		 .antMatchers("/chatsocket/**").permitAll()
+		 .antMatchers("/chatsocket/**","/app/talktochatbot/**").permitAll()
 		 
 		.anyRequest().authenticated();
 		
