b1ffb7a982cbeb99da91521eb423823fcffae445
Anees N M <aneezkhalid@gmail.com>
2022-03-25 18:02:59 +0530

    API to get doctors for Specialization

---
+++ mainProject/src/main/java/com/mentbot/mainProject/dto/AddAppointmentDto.java
@@ -0,0 +1,54 @@
+package com.mentbot.mainProject.dto;
+
+public class AddAppointmentDto {
+
+    private int specId;
+
+    private String appointmentDate;
+
+    private int doctorId;
+
+    private String startTime;
+
+    private String endTime;
+
+    public int getSpecId() {
+        return specId;
+    }
+
+    public void setSpecId(int specId) {
+        this.specId = specId;
+    }
+
+    public String getAppointmentDate() {
+        return appointmentDate;
+    }
+
+    public void setAppointmentDate(String appointmentDate) {
+        this.appointmentDate = appointmentDate;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
