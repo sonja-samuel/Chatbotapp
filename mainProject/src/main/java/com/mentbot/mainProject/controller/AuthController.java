c382200890e043e04547fa79c0525c5f32e564c6
Anees N M <aneezkhalid@gmail.com>
2022-03-28 10:17:42 +0530

    Fix role defaulting to Patient

--- mainProject/src/main/java/com/mentbot/mainProject/controller/AuthController.java
+++ mainProject/src/main/java/com/mentbot/mainProject/controller/AuthController.java
@@ -2,10 +2,10 @@ package com.mentbot.mainProject.controller;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -37,93 +37,77 @@ import javax.validation.Valid;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-	private AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
 
-	private UserRepo userRepo;
+    private UserRepo userRepo;
 
-	private RoleRepo roleRepo;
+    private RoleRepo roleRepo;
 
-	private PasswordEncoder passwordEncoder;
+    private PasswordEncoder passwordEncoder;
 
-	private JwtUtils jwtUtils;
+    private JwtUtils jwtUtils;
 
-	public AuthController(AuthenticationManager authenticationManager, UserRepo userRepo, RoleRepo roleRepo,
-			PasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
-		this.authenticationManager = authenticationManager;
-		this.userRepo = userRepo;
-		this.roleRepo = roleRepo;
-		this.passwordEncoder = passwordEncoder;
-		this.jwtUtils = jwtUtils;
-	}
+    public AuthController(AuthenticationManager authenticationManager, UserRepo userRepo, RoleRepo roleRepo,
+                          PasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.userRepo = userRepo;
+        this.roleRepo = roleRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+    }
 
-	@PostMapping("/signin")
-	public ResponseEntity<JwtResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+    @PostMapping("/signin")
+    public ResponseEntity<JwtResponse> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
 
-		Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
-		SecurityContextHolder.getContext().setAuthentication(authentication);
-		String jwt = jwtUtils.generateJwtToken(authentication);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
 
-		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-		List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
-				.collect(Collectors.toList());
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority())
+                .collect(Collectors.toList());
 
-		return ResponseEntity.ok(
-				new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
-	}
+        return ResponseEntity.ok(
+                new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles));
+    }
 
-	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
 
-		if (userRepo.existsByUsername(signUpRequest.getUsername())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken"));
-		}
+        if (userRepo.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Username is already taken"));
+        }
 
-		if (userRepo.existsByEmail(signUpRequest.getEmail())) {
-			return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already taken"));
-		}
+        if (userRepo.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Error: Email is already taken"));
+        }
 
-		User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
-				passwordEncoder.encode(signUpRequest.getPassword()), signUpRequest.getFirstname(),
-				signUpRequest.getLastname(), signUpRequest.getPhonenum(), signUpRequest.getAddressline(),
-				signUpRequest.getState(), signUpRequest.getCity(), signUpRequest.getCountry(),
-				signUpRequest.getPincode());
+        User user = new User(signUpRequest.getUsername(), signUpRequest.getEmail(),
+                passwordEncoder.encode(signUpRequest.getPassword()), signUpRequest.getFirstname(),
+                signUpRequest.getLastname(), signUpRequest.getPhonenum(), signUpRequest.getAddressline(),
+                signUpRequest.getState(), signUpRequest.getCity(), signUpRequest.getCountry(),
+                signUpRequest.getPincode());
 
-		Set<String> strRoles = signUpRequest.getRole();
-		Set<Role> roles = new HashSet<>();
+        Set<String> strRoles = signUpRequest.getRole();
+        Set<Role> roles = new HashSet<>();
 
-		if (strRoles == null) {
-			Role patientRole = roleRepo.findByName(ERole.ROLE_PATIENT)
-					.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
-			roles.add(patientRole);
-		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
-				case "admin":
-					Role adminRole = roleRepo.findByName(ERole.ROLE_ADMIN)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
-					roles.add(adminRole);
+        if (strRoles == null) {
+            Role patientRole = roleRepo.findByName(ERole.ROLE_PATIENT)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+            roles.add(patientRole);
+        } else {
+            for (String roleName : strRoles) {
+                Role role = roleRepo.findByName(ERole.getERole(roleName)).orElseThrow(() -> new RuntimeException("Error: Role is not found"));
+                roles.add(role);
+            }
+        }
+        user.setRoles(roles);
+        userRepo.save(user);
 
-					break;
-				case "doctor":
-					Role doctorRole = roleRepo.findByName(ERole.ROLE_DOCTOR)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
-					roles.add(doctorRole);
-					break;
-				default:
-					Role patientRole = roleRepo.findByName(ERole.ROLE_PATIENT)
-							.orElseThrow(() -> new RuntimeException("Error: Role is not found"));
-					roles.add(patientRole);
-				}
-			});
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
 
-		}
-		user.setRoles(roles);
-		userRepo.save(user);
-
-		return ResponseEntity.ok(new MessageResponse("User registered successfully"));
-
-	}
+    }
 
 }
--- mainProject/src/main/java/com/mentbot/mainProject/models/ERole.java
+++ mainProject/src/main/java/com/mentbot/mainProject/models/ERole.java
@@ -2,5 +2,21 @@ package com.mentbot.mainProject.models;
 
 public enum ERole {
 
-	ROLE_DOCTOR, ROLE_ADMIN, ROLE_PATIENT
+    ROLE_DOCTOR, ROLE_ADMIN, ROLE_PATIENT;
+
+    public static ERole getERole (String roleName) {
+        if (roleName == null) {
+            return null;
+        }
+        if (roleName.isEmpty()) {
+            return null;
+        }
+        for (ERole role: ERole.values()) {
+         if (role.name().equals(roleName)) {
+             return role;
+         }
+        }
+        return null;
+    }
+
 }
